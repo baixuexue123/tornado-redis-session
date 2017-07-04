9f9e016255aabfc173327c0119b53ccc1c035962
baixue <baixuexue123@hotmail.com>
2017-07-04 22:41:22 +0800

    testing

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ executor = concurrent.futures.ThreadPoolExecutor(2)
 
 class Session:
 
+    cache_key_prefix = 'session-'
+    session_id_name = '_sessionid'
+    expire_seconds = 60 * 60 * 2
+
     def __init__(self, handler):
         """
         Expects a tornado.web.RequestHandler
@@ -35,7 +39,6 @@ class Session:
         self.secret_key = handler.settings['cookie_secret']
         session_settings = handler.settings['session']
         self.engine = session_settings['engine']
-        self.cache_key_prefix = session_settings['cache_key_prefix']
         self.expire_seconds = session_settings['expire_seconds']
         self.session_id_name = session_settings['session_id_name']
 
@@ -54,7 +57,15 @@ class Session:
 
     def _get_new_session_id(self):
         session_id = str(uuid4())
+        self.handler.set_secure_cookie(self.session_id_name, session_id,
+                                       **self._cookie_settings())
         return session_id
+
+    def _cookie_settings(self):
+        cookie_settings = self.settings.get('cookies', {})
+        cookie_settings.setdefault('expires', None)
+        cookie_settings.setdefault('expires_days', None)
+        return cookie_settings
 
     def _get_session(self, no_load=False):
         """
@@ -74,34 +85,14 @@ class Session:
     _session = property(_get_session)
 
     def load(self):
-        try:
-            session_data = self.engine.get(self.cache_key)
+        session_data = self.engine.get(self.cache_key)
         if session_data is not None:
-            return session_data
-        self._session_id = None
+            return pickle.loads(session_data)
         return {}
 
-    def save(self, must_create=False):
-        if self.session_id is None:
-            return self.create()
-        if must_create:
-            func = self._cache.add
-        elif self._cache.get(self.cache_key) is not None:
-            func = self._cache.set
-        else:
-            raise UpdateError
-        result = func(self.cache_key,
-                      self._get_session(no_load=must_create),
-                      self.get_expiry_age())
-        if must_create and not result:
-            raise CreateError
-
-    def delete(self, session_key=None):
-        if session_key is None:
-            if self._session_id is None:
-                return
-            session_key = self._session_id
-        self.engine.delete(self.cache_key_prefix + session_key)
+    def save(self):
+        session_data = pickle.dumps(self._session)
+        self.engine.set(self.cache_key, session_data, self.expire_seconds)
 
     def update(self, dict_):
         self._session.update(dict_)
@@ -123,6 +114,9 @@ class Session:
 
     def get(self, key, default=None):
         return self._session.get(key, default)
+
+    def pop(self, key, default=None):
+        return self._session.pop(key, default)
 
     def has_key(self, key):
         return key in self._session
@@ -149,13 +143,6 @@ class Session:
         self._session_cache = {}
         self.accessed = True
         self.modified = True
-
-    def is_empty(self):
-        "Returns True when there is no session_key and the session is empty"
-        try:
-            return not bool(self._session_key) and not self._session_cache
-        except AttributeError:
-            return True
 
 
 
@@ -285,7 +272,6 @@ class Application(tornado.web.Application):
         settings['session'] = dict(
             session_id_name='_sessionid',
             expire_seconds=60 * 60 * 1,
-            cache_key_prefix='session-',
             engine=redis.StrictRedis(host='127.0.0.1', port=6379, db=0),
         )
         super(Application, self).__init__(handlers, **settings)
